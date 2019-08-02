f7d31d0b25c638dcc9ba7b219e74a2b481084ba0
RXstoinkboy <erykludwin@gmail.com>
2019-08-03 01:46:14 +0200

    stopped working on jasmine tests. Moved to front end. Implemented sass into the project. WOrking on layout now

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 const router = require('./routes/router');
 const auth = require('./middlewares/auth');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
+const fs = require('fs');
+const sass = require('node-sass');
 
 const port = process.env.PORT || 8080;
 
@@ -12,14 +15,32 @@ app.set('view engine', 'pug');
 
 app.use(cookieParser());
 app.use(express.urlencoded({extended: true}));
+app.use(compression());
+app.use(express.static('public'));
+
+sass.render({
+    file: './src/scss/style.scss',
+    outFile: './public/css/style.css',
+    outputStyle: 'compressed',
+    sourceMap: true
+  }, function(err, result) {
+      if(!err){
+          fs.writeFile('./public/css/style.css', result.css, (err, result)=>{
+              if(!err){
+                  console.log(result)
+              }
+          })
+      }
+  });
 
 // apply router middleware
 app.use('/users', router.users);
 app.use('/tasks', router.tasks);
 app.use('/lists', router.lists);
 
-app.get('/', auth, (req, res) => {
-    res.status(200).send('Hello world!');
+app.get('/', (req, res) => {
+    res.render('home')
+    // res.status(200);
 })
 
 // UPDATE IT TO SHOW 404 PAGE
--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,43 +1,66 @@
 const request = require('request');
-const app = require('../server');
+const db = require('../config/db');
+// const createNewUser = request('../routes/queries/createNewUser.js');
 
 const domain = `http://localhost:8080/users`;
 
 describe('POST /users', ()=>{
-
-    const forms = {
-        wrongData: {
-            username: 'testUser123',
-            password: 'pass123'
-        },
-        correctData: {
-            username: 'testUser123',
-            password: 'testPass123'
-        }
+    const validInput = {
+        username: 'testUser1',
+        password: 'testPassword123'
+    };
+    
+    const wrongInput = {
+        username: 'testUser1',
+        password: 'wrong'
     };
 
-    afterAll((done)=>{
-        request.delete(domain, {form: {username: forms.correctData.username}}, (err, res, body)=>{
-            expect(body).toBe('user deleted');
-            done();
+
+    beforeEach((done)=>{
+        const getUserInfo = `
+            SELECT * FROM users
+                WHERE username = $1
+        `;
+
+        const deleteUser = `
+            DELETE FROM users
+                WHERE username = $1
+        `;
+
+        // delete user from database  after tests are done
+        db.query(getUserInfo, [validInput.username], (error, results) => {
+            console.log(results);
+
+            // const user = results.rows;
+            if(error) console.log(error);
+
+            // if(user){
+            //     return db.query(deleteUser, [validInput.username], (error, results) => {
+            //         console.log(validInput.username, ' was deleted after tests.')
+            //     })
+            // }
         })
+        done();
     })
 
-    it(`doesn't allow to register if password doesn't match requirements` , (done)=>{
-        
-        request.post(domain, {form: forms.wrongData}, (err, res, body)=>{
+    it('should return 400 and NOT create user if data was not valid', (done) => {
+        request.post(domain, wrongInput, (error, res, body)=>{
+            // if(error) console.log(error);
+
             expect(res.statusCode).toBe(400);
-            expect(body).toContain('provide correct');
-            done();
+            expect(body).toContain('Please provide');
         })
+        done();
     })
 
-    it(`creates new user if data is in required format`, (done)=>{
-        request.post(domain, {form: forms.correctData}, (err, res, body)=>{
+    it('should return 200 and create new user if data was correct' , (done)=>{
+        request.post({url: domain, form: validInput}, (err, res, body)=>{
+            // if(err) console.log('error: ' + err);
+
             expect(res.statusCode).toBe(200);
-            expect(body).toBe(`User ${forms.correctData.username} was successfully created!`);
-            console.log(body);
+            expect(body).toBe(`User ${validInput.username} was successfully created!`);
             done();
-        })
+        });
     })
+
 })
